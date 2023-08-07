df3065a87096056389570af9c229d939a4bdacfd
Omotayo <kehindeomotayo11@gmail.com>
2023-08-07 18:06:57 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+console.log(mod);
+
+arr = [4, 8, 12, 20];
+let max = Math.max(...arr);
+console.log(max);
